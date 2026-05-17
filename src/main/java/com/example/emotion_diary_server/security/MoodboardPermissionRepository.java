049535d4e7e44cb5016109708d48faf3084fb303
Tomas Robino <tomaserobino@gmail.com>
2026-05-17 20:08:14 +0200

    delete moodboard permission working

--- src/main/java/com/example/emotion_diary_server/security/MoodboardPermissionRepository.java
+++ src/main/java/com/example/emotion_diary_server/security/MoodboardPermissionRepository.java
@@ -1,10 +1,11 @@
 package com.example.emotion_diary_server.security;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MoodboardPermissionRepository extends JpaRepository<MoodboardPermission, Long> {
 
     boolean existsByOwnerUsernameAndPermittedUsername(String ownerUsername, String permittedUsername);
-
+    @Transactional
     void deleteByOwnerUsernameAndPermittedUsername(String ownerUsername, String permittedUsername);
 }
